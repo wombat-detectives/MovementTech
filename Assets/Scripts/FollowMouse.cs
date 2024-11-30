1dfded04a63ee65e47a9884f2a47c07db5ff2e30
Maguire <mac.richard@live.com>
2024-11-30 16:03:47 -0400

    Added Hubert Head to menu

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,23 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public Rigidbody targetRB;
+    public Camera cam;
+
+    public float followForce = 1f;
+    public float lookZOffset = 1f;
+
+    void Update()
+    {
+        Vector3 targetPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetRB.position.z));
+
+        Vector3 forceToApply = (targetPos - targetRB.position) * followForce;
+
+        targetRB.AddForce(forceToApply, ForceMode.Force);
+            
+        //targetRB.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetRB.position.z));
+        targetRB.transform.LookAt(cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetRB.position.z + lookZOffset)));
+    }
+}
